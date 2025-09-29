e412d1f95975ef05abde2f3c0b628b7d1e675370
Abderahman <abdelrahman16174@gmail.com>
2025-09-29 10:22:23 +0300

    Improvement url filters

--- core/templatetags/query_string.py
+++ core/templatetags/query_string.py
@@ -6,6 +6,10 @@ register = template.Library()
 @register.simple_tag
 def query_string(request, **kwargs):
     income = request.GET.copy()
+    for key in list(income.keys()):
+        if not income[key]:
+            del income[key]
+
     for key, value in kwargs.items():
         if value is not None and value != "":
             income[key] = str(value)
